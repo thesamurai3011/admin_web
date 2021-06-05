48e0378315847d68d6ba9bad1f0cfc0ddee22f54
thesamurai3011 <thesamurai3011@gmail.com>
2021-06-05 10:27:23 +0700

    fix add email

--- js/addUser.js
+++ js/addUser.js
@@ -9,17 +9,23 @@ var submitBtn = document.getElementById("addBtn");
 
 function add(){
     var ref = firebase.database().ref().child("User");
-    var newUser = ref.push();
-    newUser.set({
-        email: text1.value,
-        password: text2.value,
-        phone: text3.value,
-        address: text4.value,
-        pin: text5.value,
-        id: text6.value,
+    ref.orderByChild("email").equalTo(text1.value).once("value", snapshot => {
+        if(snapshot.exists()){
+            alert("exists email");
+        }
+        else{
+            var newUser = ref.push();
+            newUser.set({
+                email: text1.value,
+                password: text2.value,
+                phone: text3.value,
+                address: text4.value,
+                pin: text5.value,
+                id: text6.value,
+            });
+            alert("Add user: " + text1.value);
+        }
     });
-    
-    alert("Add user: " + text1.value);
 }
 
 function cancel(){
