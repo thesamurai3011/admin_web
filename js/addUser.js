6d7937c1e67d41463edb4ca0665213dfdde78cc6
thesamurai3011 <thesamurai3011@gmail.com>
2021-06-12 11:28:15 +0700

    change

--- js/addUser.js
+++ js/addUser.js
@@ -1,11 +1,13 @@
 var text1 = document.getElementById("email");
-var text2 = document.getElementById("password");
+// var text2 = document.getElementById("password");
+//default password = admin
+var text2 = "admin";
 var text3 = document.getElementById("phone");
 var text4 = document.getElementById("address");
 var text5 = document.getElementById("pin");
 var text6 = document.getElementById("id");
 var submitBtn = document.getElementById("addBtn");
-
+var component;
 
 function add(){
     var ref = firebase.database().ref().child("User");
@@ -17,16 +19,54 @@ function add(){
             var newUser = ref.push();
             newUser.set({
                 email: text1.value,
-                password: text2.value,
+                password: text2,
                 phone: text3.value,
                 address: text4.value,
                 pin: text5.value,
                 id: text6.value,
             });
+            component = newUser.key;
+            
+            
+            //Thêm component
+            firebase.database().ref('Component/' + component).set({
+               Sensor: {
+                   0: {
+                       deviceID: "sensor_1",
+                       mode: 2,
+                       name: "sensor_1",
+                       position: 0,
+                   },
+                   1: {
+                       deviceID: "sensor_2",
+                       mode: 2,
+                       name: "sensor_2",
+                       position: 1,
+                   },
+               },
+               Servo: {
+                   0: {
+                       deviceID: "servo_1",
+                       name: "Cua chinh",
+                       state: false,
+                   },
+               },
+               Speaker: {
+                   0: {
+                       deviceID: "speaker_1",
+                       name: "Loa chong trom",
+                       volume: 51,
+                   },
+               },
+            });
             alert("Add user: " + text1.value);
         }
     });
+    
 }
+
+
+
 
 function cancel(){
     location.replace("main.html");
--- js/response.js
+++ js/response.js
@@ -1,35 +1,43 @@
 
 var rootRef = firebase.database().ref().child("Response");
 var rootRef2 = firebase.database().ref().child("User");
-var app = {};
+var app;
 
-
-// rootRef.on("child_added", snap => {
-//     var score = snap.child("score").val();
-//     var text = snap.child("text").val();
-//     $("#table_body").append("<tr><td>" +  score + "</td><td>" + text + "</td></tr>");
-
-// });
+var aRef1 = firebase.database().ref("Response/App");
 
 function fetchData(){
-    rootRef.once('value', function(snapshot){
-        snapshot.forEach(
-            function(ChildSnapshot){
-                var score = ChildSnapshot.val().score;
-                var text = ChildSnapshot.val().text;
-                var key = ChildSnapshot.key;
-                // alert(key);
-                var emailRef = firebase.database().ref('User/' + key + '/email');
-                var email = {};
-                emailRef.once('value', (snapshot) => {
-                    email = snapshot.val();
-                    // alert(email);
-                    $("#table_body").append("<tr><td>" +  email + "</td><td>" + score + "</td><td>" + text + "</td></tr>");
-                });
+    // rootRef.once('value', function(snapshot){
+    //     snapshot.forEach(
+    //         function(ChildSnapshot){
+    //             var score = ChildSnapshot.val().score;
+    //             var text = ChildSnapshot.val().text;
+    //             var key = ChildSnapshot.key;
+    //             // alert(key);
+    //             var emailRef = firebase.database().ref('User/' + key + '/email');
+    //             var email = {};
+    //             emailRef.once('value', (snapshot) => {
+    //                 email = snapshot.val();
+    //                 // alert(email);
+    //                 $("#table_body").append("<tr><td>" +  email + "</td><td>" + score + "</td><td>" + text + "</td></tr>");
+    //             });
                 
+    //         }
+    //     );
+    // });
+    
+    /*Response for App*/
+
+    aRef1.once("value", function (snapshot) {
+        snapshot.forEach(
+            function (ChildSnapshot){
+                var email = ChildSnapshot.val().email;
+                var text = ChildSnapshot.val().text;
+                var score = ChildSnapshot.val().score;
+                $("#table_body").append("<tr><td>" +  email + "</td><td>" + score + "</td><td>" + text + "</td></tr>");
             }
         );
     });
+
 }
 
 document.getElementById("returnBtn").onclick = function(){myfunction()};
